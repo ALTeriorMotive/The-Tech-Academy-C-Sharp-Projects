21147f9d0a9e64b0b5a91a88fbd8d3c3d8d6dc63
ALTeriorMotive <lightning.aaron1221@gmail.com>
2025-07-05 13:36:58 -0500

    Submitted Branching Assignment

---
+++ Branching_Assignment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BranchingSubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            float weight = Convert.ToSingle(Console.ReadLine());
+            if(weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width:");
+                float width = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Please enter the package height");
+                float height = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:");
+                float length = Convert.ToSingle(Console.ReadLine());
+                if((width + height + length) > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    string quote = Convert.ToString((height * width * length * weight) / 100);
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
