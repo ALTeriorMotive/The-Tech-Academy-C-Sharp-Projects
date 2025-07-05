f107e96f591472ccc9927c0d13ed211c7c78d1fd
ALTeriorMotive <lightning.aaron1221@gmail.com>
2025-07-05 13:04:07 -0500

    Submitted Boolean Logic Assignment

---
+++ Boolean_Logic_Assignment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BooleanLogicAssignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            byte age = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please enter 'true' or 'false.'");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            byte ticketNum = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            Console.WriteLine((age > 15) && (dui == false) && (ticketNum <= 3));
+            Console.ReadLine();
+
+        }
+    }
+}
